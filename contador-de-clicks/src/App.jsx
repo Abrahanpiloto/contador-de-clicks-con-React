8e20b0fc9381bca709668999b9f26a22343c8dc5
abrahan <abrahanpiloto@gmail.com>
2023-04-02 17:49:06 -0500

    finalizado

---
+++ contador-de-clicks/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import {useState} from 'react';
+
+
+function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  };
+  
+  return (
+    <div className='App'>
+      <div className='contenedor-titulo'>
+        <h1>Contador de Clics</h1>
+        <p>Con React</p>
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClicks={numClicks} />
+        <Boton
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador} />
+      </div>
+    </div>
+  )
+}
+
+export default App
